4c7f7052c3f3181054ef17381cccf04720b259e7
Christine Onita <79072825+christineonita@users.noreply.github.com>
2023-06-18 09:23:28 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var explorerecipesbutton = document.getElementById("explorerecipesbutton");
+  explorerecipesbutton.addEventListener("click", function () {
+    window.location.href = "recipes.html";
+  });
+});
